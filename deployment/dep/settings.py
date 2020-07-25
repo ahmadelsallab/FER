5051de18a9653dff3bc59b499c70965e8d0e2499
Ahmad El Sallab <ahmad.elsallab@gmail.com>
2020-07-25 16:56:12 +0200

    Back to git

--- deployment/dep/settings.py
+++ deployment/dep/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lqj8(xgotp0n^=$rxqp))m!$mvm=3d2z3kpss%@g5inzm!gq^g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- deployment/dep/wsgi.py
+++ deployment/dep/wsgi.py
@@ -7,9 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append('/home/ahmad/anaconda3/lib/python3.7')
+#import os
+#print(os.system('which python'))
+
+import subprocess
+result = subprocess.run(['which', 'python'], stdout=subprocess.PIPE)
+
+print(sys.stderr.write("*******************"))
+print(sys.stderr.write(str(result.stdout)))
 
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dep.settings')
 
